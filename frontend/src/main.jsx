b0d7bd714c340159bc2580385285ab7243a6bf53
Gaurav Mehra <151906528+Gauravmehra88@users.noreply.github.com>
2026-08-05 13:39:59 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,5 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'remixicon/fonts/remixicon.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-
-  <App />
-
-)
+createRoot(document.getElementById("root")).render(
+  <h1>Hello World</h1>
+);
